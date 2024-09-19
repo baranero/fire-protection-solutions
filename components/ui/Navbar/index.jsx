f9b605ec39c235f5abcd4a5d49c3c3ee2b712db6
baranero <jakub.baran2@gmail.com>
2024-09-19 21:20:43 +0200

    up

--- components/ui/Navbar/index.jsx
+++ components/ui/Navbar/index.jsx
@@ -145,7 +145,7 @@ const Navbar = () => {
                             })}
                             <li>
                                 <NavLink
-                                    href="#kontakt"
+                                    href="/#kontakt"
                                     className="block font-medium text-sm text-white bg-gray-800 hover:bg-gray-600 active:bg-gray-900 md:inline"
                                 >
                                     Kontakt
